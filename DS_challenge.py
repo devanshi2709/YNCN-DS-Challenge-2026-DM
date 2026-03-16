092bb68575098b50d54f78d37c7d78a290ef09c3
Jie Xuan Liu <baronliu1993@gmail.com>
2026-03-15 23:46:45 -0400

    init

---
+++ DS_challenge.py
@@ -0,0 +1,32 @@
+# CODE STARTS HERE
+
+
+
+
+
+
+
+
+
+
+# CODE ENDS HERE
+    
+# REPORT YOUR VALUES IN THE DOCSTRING BELOW
+
+"""
+Which 5 events had the highest attendance?
+Answer:
+
+
+
+How many new attendees were there by event type?
+Answer:
+
+
+
+What data quality issues exist in the dataset, and how would you handle them before analyzing attendance?
+Answer: 
+
+
+
+"""
